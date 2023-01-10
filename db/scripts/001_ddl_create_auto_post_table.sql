361079db43889c2b1dbc5995abc3dbb7b82e2932
Liana <95958723+Lianatim@users.noreply.github.com>
2023-01-10 21:56:59 +0300

    Модели и связи. Машины и владельцы

--- db/scripts/001_ddl_create_auto_post_table.sql
+++ db/scripts/001_ddl_create_auto_post_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE auto_post
 (
     id           SERIAL PRIMARY KEY,
-    text         TEXT      NOT NULL,
+    description        TEXT      NOT NULL,
     created      TIMESTAMP NOT NULL,
     auto_user_id INT       NOT NULL REFERENCES auto_user (id)
 );
